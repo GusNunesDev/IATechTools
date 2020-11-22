56da8c1cc3dc35b66832a734936ae97b22a906b9
Gustavo Nunes <gustavo_anunes@hotmail.com>
2020-11-21 23:17:29 -0300

    Click longo apaga os itens do todo, foi criada a documentação do TodolistActivity e strings.xml, foi alterado o layout da caulculadora, e açterado a cor dos botões

--- app/src/main/java/br/uninove/iatechtools/ToDoListActivity.java
+++ app/src/main/java/br/uninove/iatechtools/ToDoListActivity.java
@@ -2,9 +2,9 @@ package br.uninove.iatechtools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,21 +26,37 @@ public class ToDoListActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todolist);
 
+        //Atribuindo valores dos botões ás variáveis locais
         editToDo = findViewById(R.id.editToDo);
         btAdicionarToDo = findViewById(R.id.btAdicionarToDo);
         btLimpaListaToDo = findViewById(R.id.btLimparListaToDo);
         listaToDo = findViewById(R.id.listaToDo);
 
+        //Array que armazena as strings
         lista = new ArrayList<>();
 
+        //Array adaptador que busca o layout da lista Array atualizado
         adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista);
         listaToDo.setAdapter(adaptador);
 
+        //Listener que recebe o conteudo da EditText
         btAdicionarToDo.setOnClickListener(this);
         btLimpaListaToDo.setOnClickListener(this);
 
+        //Função para apagar os itens dentro do listView, com long click
+        listaToDo.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View v, int pos, long id) {
+
+                lista.remove(pos);
+                adaptador.notifyDataSetChanged();
+
+                return false;
+            }
+        });
     }
 
+    //Adiciona os textos no array lista
     @Override
     public void onClick(View v){
         if(v == btAdicionarToDo){
@@ -52,11 +68,11 @@ public class ToDoListActivity extends AppCompatActivity implements View.OnClickL
                 editToDo.setText("");
                 editToDo.requestFocus();
             }else{
-                Toast.makeText(getApplicationContext(),
-                        "Campo vazio, digite algo!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Campo vazio, digite algo!", Toast.LENGTH_LONG).show();
             }
         }
 
+        //Botão limpar a lista, exclui limpa o array
         if(v == btLimpaListaToDo){
             lista.clear();
             adaptador.notifyDataSetChanged();
