32ee5205673fa6b46fbe1defb88b3f16651103bb
Gustavo Nunes <gustavo_anunes@hotmail.com>
2020-11-26 22:00:10 -0300

    Activity criação e lista está quase pronto

--- app/src/main/java/br/uninove/iatechtools/NotalistaActivity.java
+++ app/src/main/java/br/uninove/iatechtools/NotalistaActivity.java
@@ -24,7 +24,7 @@ public class NotepadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notepadlista);
     }
 
     public void fabNovaNotaClick(View v){
